6e894c18e2011d88fd7c4dbf3bcd7bce23b5973f
Roman Bereziuk <roma.physicist@gmail.com>
2019-10-03 00:41:49 +0300

    isPalindrom

---
+++ src/algorithms/isPalindrom.js
@@ -0,0 +1,28 @@
+function isPalindrom(str) {
+  const strReverse = str
+    .toLowerCase()
+    .split('')
+    .reverse()
+    .join('');
+
+  if (str == strReverse) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isPalindrom2(string) {
+  string = string.toLowerCase();
+  var charactersArr = string.split('');
+  var validCharacters = 'abcdefghijklmnopqrstuvwxyz';
+
+  var lettersArr = [];
+  charactersArr.forEach(char => {
+    if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
+  });
+
+  if (lettersArr.join('') == lettersArr.reverse().join('')) return true;
+}
+
+console.log(isPalindrom2("Madam I'm Adam"));
